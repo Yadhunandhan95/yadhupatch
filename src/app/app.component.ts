8a7194e76b3bf0a8b830f614765fef18c9388b36
Ashokkumar S <arvindbtch@gmail.com>
2023-04-09 20:02:50 +0530

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'formValidation';
+export class AppComponent implements OnInit {
+  public heroForm:FormGroup;
+  public showData:boolean = false;
+
+  constructor(private fb:FormBuilder){
+  }
+  ngOnInit(): void {
+    this.heroForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)//
+      ]),
+      email: new FormControl('',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+    });
+
+  }
+
+  get name() { return this.heroForm.get('name'); }
+
+  get email() { return this.heroForm.get('email'); }
 }
