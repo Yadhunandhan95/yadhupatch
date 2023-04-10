281b2874850a71b5b2725876dd55160e84c24404
Raghul <raghul@ecbee.net>
2023-04-10 18:37:56 +0530

    https://github.com/Yadhunandhan95/toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { getLocaleDayNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators,FormControl } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-root',
@@ -7,23 +9,49 @@ import { FormGroup,FormBuilder,Validators,FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public heroForm:FormGroup;
-  public showData:boolean = false;
+  yadhuForm:FormGroup;
+  showData:boolean = false;
+  public value!: string;
 
-  constructor(private fb:FormBuilder){
-  }
+  names = ['Sedhu','Sanjay','Aravinth','Prabhu'];
+ 
+
   ngOnInit(): void {
-    this.heroForm = new FormGroup({
+    function my(){
+       var i: any
+      if (i === getLocaleDayNames.length) {
+       const i = 0;
+      
+      }
+    }
+    this.yadhuForm = new FormGroup({
       name: new FormControl('', [
         Validators.required,
         Validators.minLength(4)//
       ]),
-      email: new FormControl('',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+      number: new FormControl('',[
+        Validators.required,
+        Validators.pattern('[0-9]'),
+        Validators.minLength(10)//
+      ]),
+      email: new FormControl('',[
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z]+.[a-z]{2,3}$')])
     });
 
   }
 
-  get name() { return this.heroForm.get('name'); }
+  deleteTask(_names:any) {
+    console.log('going to delete-',_names)
+    var _: any
+    var values = _.findIndex(this.names,_names)
+    this.names.splice(values,1);
+  }
+ 
+  get name() { return this.yadhuForm.get('name'); }
+  get number() { return this.yadhuForm.get('number'); }
+  get email() { return this.yadhuForm.get('email'); }
 
-  get email() { return this.heroForm.get('email'); }
 }
+
