afa8e52f08386e6788a0689f49bfacf47972fe63
Raghul <raghul@ecbee.net>
2023-04-13 13:39:42 +0530

    patch added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { getLocaleDayNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators,FormControl } from '@angular/forms';
 import { __values } from 'tslib';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,43 @@ export class AppComponent implements OnInit {
   yadhuForm:FormGroup;
   showData:boolean = false;
   public value!: string;
-
-  names = ['Sedhu','Sanjay','Aravinth','Prabhu'];
- 
+ item_names = [
+    "Sanjay",
+    "Sedhu",
+    "Aravinth",
+    "Prabhu",
+    "Prem",
+    "JayaPrasath",
+    "Sabari",
+    "Ram",
+    "Kumar",
+    "Gowtham"
+    ]
+    // item_number = [
+    // "9098786865",
+    // "9098786908",
+    // "9098345963",
+    // "9898786455",
+    // "8768986465",
+    // "9983475084",
+    // "7892365024",
+    // "8279346201",
+    // "9823623402",
+    // "7234952695"
+    // ]
+    // item_email = [
+    //   "sanjay@link.com",
+    //   "sedhu@kin.com",
+    //   "arvinth@gmail.com",
+    //   "prabhu@fun.com",
+    //   "prem@job.com",
+    //   "jp@gin.com",
+    //   "sabari@eol.com",
+    //   "ram@qli.com",
+    //   "kumar@right.com",
+    //   "gowtham@yarn.com"
+    // ]
+  isValidFormSubmitted: boolean;
 
   ngOnInit(): void {
     function my(){
@@ -24,6 +59,7 @@ export class AppComponent implements OnInit {
       
       }
     }
+   
     this.yadhuForm = new FormGroup({
       name: new FormControl('', [
         Validators.required,
@@ -41,17 +77,38 @@ export class AppComponent implements OnInit {
     });
 
   }
-
-  deleteTask(_names:any) {
-    console.log('going to delete-',_names)
-    var _: any
-    var values = _.findIndex(this.names,_names)
-    this.names.splice(values,1);
+  get name() 
+  {
+    return this.yadhuForm.get('name');
   }
- 
-  get name() { return this.yadhuForm.get('name'); }
-  get number() { return this.yadhuForm.get('number'); }
-  get email() { return this.yadhuForm.get('email'); }
+  get number()
+  {
+    return this.yadhuForm.get('number');
+  }
+  get email()
+  {
+    return this.yadhuForm.get('email');
+  }
 
-}
+      patchUserValues() {
+        
+      
+        this.yadhuForm.patchValue({
+          
+          name: this.item_names[0]
+          // , number: this.item_number[0], email: this.item_email[0]
+          
+        });
+        var name_index = _.findIndex(this.item_names)
+        // var number_index = _.findIndex(this.item_number)
+        // var email_index = _.findIndex(this.item_email)
+        this.item_names.splice(name_index,1);
+        // this.item_number.splice(number_index,1);
+        // this.item_email.splice(email_index,1);
+      }
+      
+  }
+    
 
+
+    
