c4a056bc8c5f4150bc6b134773613896dff01ac4
Yadhunandhan95 <115917724+Yadhunandhan95@users.noreply.github.com>
2023-04-19 15:54:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,26 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators,FormControl, FormArray } from '@angular/forms';
 import { __values } from 'tslib';
 import * as _ from 'underscore';
-// interface Info {
-//   content: string;
-//   id?: string;
-//   datemodified?: Date;
-//   isDone?: boolean;
-// }
+interface Info {
+  content: string;
+  id?: string;
+  datemodified?: Date;
+  isDone?: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  
   items:FormArray;
   yadhuForm:FormGroup;
+  
   showData:boolean = false;
   public value!: string;
+  listData: any = [];
+  displayval = "";
   
  item_names = [
     "Sanjay",
@@ -58,12 +62,25 @@ export class AppComponent implements OnInit {
     // ]
   isValidFormSubmitted: boolean;
   ind: any;
+  
+  names1:any
+  numbers1:any
+  emails1:any
+  secnum:any
   i = -1;
  box = document.getElementById('box');
   info_input: any;
-  infoCollection: any;
   editValue: boolean = false;
+  infoCollection: Info[] = [];
+
+  list_input:Info = {
+    content:""
+  };
   inputId: any='';
+  str: any;
+  yad1: any;
+item: any;
+  e : any;
   ngOnInit(): void {
 
 
@@ -83,7 +100,7 @@ export class AppComponent implements OnInit {
         Validators.email,
         Validators.pattern('^[a-z0-9._%+-]+@[a-z]+.[a-z]{2,3}$')]),
        
-        secondaryNumber: this.fb.array([])
+        secondaryNumber: new FormArray([])
         
     })
   
@@ -107,7 +124,17 @@ export class AppComponent implements OnInit {
   }
 
   
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+
+  
+
+    this.yadhuForm = this.fb.group({
+      name: ['', Validators.required],
+      number: ['', Validators.required],
+      secondaryNumber: ['', Validators.required],
+      email: ['', Validators.required],
+    })
+  }
   
  nextName() {
       this.i = this.i >= this.item_names.length - 1 ? 0 : this.i + 1;
@@ -117,47 +144,69 @@ export class AppComponent implements OnInit {
      });
       console.log("test", this.item_names[this.i])
     }
-    addSecondaryNumber() {
+     addSecondaryNumber() {
       this.secondaryNumber.push(this.fb.control(''));
       }
       deleteSecondaryNumber(index: number) {
         this.secondaryNumber.removeAt(index);
       }
-      // addInfo() {
-      //   if (this.info_input.content != "") {
-      //     var inputValue: Info={
-      //       content:""
-      //     };
-      //     inputValue.datemodified = new Date();
-      //     inputValue.isDone = false;
-      //     inputValue.content = this.info_input.content;
-      //     inputValue.id = _.uniqueId('info_');
-      //     this.infoCollection.push(inputValue);
-      //     this.info_input.content = ""
-      //     console.log('added- ',this.infoCollection)
-      //     this.openSnackBar("Added Successfuly!", "Dismiss");
-      //   }
-      // }
-      // deleteItem(info_:any) {
-      //   console.log('going to delete-',info_)
-      //   var info_index = _.findIndex(this.infoCollection,info_)
-      //   console.log('todo_index del-',info_index)
-      //   this.infoCollection.splice(info_index,1);
-      //   this.openSnackBar("Item Deleted!", "Dismiss");
-      // }
-      // editItem(info:any) {
-      //   var inputValue: Info={
-      //     content:""
-      //   };
-      //   this.info_input.content = info.content;
-      //   this.editValue = true;
-      //   this.inputId = info.id;
-      // }
 
+    AddItem(){
+      // alert(this.yadhuForm.value.name)
+     
+      this.listData.push({"name":this.names1,"number":this.numbers1,"secondaryNumber":this.secnum,"email":this.emails1})
 
-  // openSnackBar(arg0: string, arg1: string) {
-  //   throw new Error('Method not implemented.');
-  // }
-      
-    } 
+    }
     
+    Reset(){
+      this.yadhuForm.reset();
+    }
+
+
+    // removeItem(){
+    //   var list_index = this.listData.length;
+    //   this.listData.splice(list_index,1);
+    // }
+    deleteItem(item:any) {
+      console.log('going to delete-',item)
+      var index = _.findIndex(this.listData,item)
+    //   let index = this.listData.findIndex(e => e.item === item);
+    //  if(index !== -1){
+    //   this.listData.splice(index,1);
+    //  }
+     
+    //  console.log(index); 
+      console.log('list_index -del',index)
+      this.listData.splice(index,1);
+      
+    }
+    editItem(item: any) {
+      debugger;
+      this.listData.forEach((element: { isEdit: boolean; }) => {
+        element.isEdit = false;
+      });
+      item.isEdit = true;
+    }
+      
+    saveNewItem() {
+      var inputValue: Info={
+        content:""
+      };
+      if (this.listData.content != "") {
+        inputValue.isDone = false;
+        inputValue.datemodified = new Date();
+        inputValue.content = this.listData.content
+        var temp_id = this.inputId;
+        
+  
+        var temp_index=_.findIndex(this.listData,{id:temp_id} )
+        this.listData[temp_index] = inputValue
+        this.editValue = false;
+        this.listData.content = "";
+        this.inputId = '';
+        
+      }
+    }
+    }
+  
+  
